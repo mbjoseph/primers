6805cb26159b784e244d273676ff518ce1242944
Max Joseph <maxwell.b.joseph@colorado.edu>
2019-02-20 09:21:24 -0700

    Download and process park files in parallel

--- create-park-masks.R
+++ create-park-masks.R
@@ -3,18 +3,20 @@ library(raster)
 library(tidyverse)
 library(viridis)
 library(rgeos)
-library(ggrepel)
 
 d <- read_delim('data/macav2metdata_urls.txt', delim='\n', col_names = 'url')
 
-download.file(d$url[1], destfile='out.nc')
 
-r <- raster('out.nc')
+destf <- trimws(basename(d$url[1]))
+download.file(d$url[1], destfile = destf)
+
+r <- raster(destf)
 er <- extent(r)
 extent(r) <- c(er@xmin - 360, 
                er@xmax - 360, 
                er@ymin, 
                er@ymax)
+projection(r) <- "+proj=longlat +ellps=WGS84 +no_defs"
 
 plot(r, col = viridis(100))
 
@@ -37,51 +39,20 @@ plot(r, col = viridis(100), axes = FALSE, box = FALSE, legend = FALSE)
 plot(national_parks, add = TRUE)
 
 
-
-plot(trim(mask(r, national_parks[36, ])), col = viridis(100), axes = FALSE, 
-     box = FALSE, legend = FALSE)
-
-plot(trim(mask(r, national_parks[46, ])), col = viridis(100), axes = FALSE, 
-     box = FALSE, legend = FALSE)
-
-plot(trim(mask(r, national_parks[17, ])), col = viridis(100), axes = FALSE, 
-     box = FALSE, legend = FALSE)
-
-
-park_rasters <- list()
+# Iterate over parks, saving raster versions of each ----------------------
+pb <- txtProgressBar(max = nrow(national_parks), style = 3)
 for (i in seq_len(nrow(national_parks))) {
-  park_rasters[[i]] <- rasterize(national_parks[i, ], r)
-  all_na <- all(is.na(values(park_rasters[[i]])))
+  park_r <- rasterize(national_parks[i, ], r)
+  all_na <- all(is.na(values(park_r)))
   if (all_na) {
     centroid <- gCentroid(national_parks[i, ])
-    park_rasters[[i]] <- rasterize(centroid, r)
+    park_r <- rasterize(centroid, r)
     print('Using point representation of:')
-    print(national_parks$UNIT_NAME[i])
+    print(as.character(national_parks$UNIT_NAME[i]))
   }
-  park_rasters[[i]] <- trim(park_rasters[[i]])
+  code <- national_parks$UNIT_CODE[i]
+  writeRaster(trim(park_r), 
+              filename = file.path('data/masks', paste0(code, '.tif')), 
+              overwrite = TRUE)
+  setTxtProgressBar(pb, i)
 }
-names(park_rasters) <- national_parks$UNIT_CODE
-
-dir.create('data/masks')
-for (i in seq_along(park_rasters)) {
-  code <- names(park_rasters)[i]
-  writeRaster(park_rasters[[i]], filename = file.path('data/masks', 
-                                                      paste0(code, '.tif')))
-}
-
-
-# get number of pixels per park
-pixel_df <- lapply(park_rasters, function(x) sum(values(x), na.rm = TRUE)) %>%
-  unlist %>%
-  enframe(name = 'UNIT_CODE') %>%
-  arrange(value) %>%
-  mutate(order = 1:n()) %>%
-  left_join(as.data.frame(national_parks))
-
-pixel_df %>%
-  ggplot(aes(value, order)) + 
-  geom_point() + 
-  geom_text_repel(aes(label = UNIT_NAME), data = filter(pixel_df, order > 44), 
-                  nudge_y = -15) +
-  ylab('Order (smallest to largest)') + 
-  xlab('Number of MACA pixels')
--- maca-exploration.R
+++ maca-exploration.R
@@ -1,48 +1,62 @@
 library(raster)
-library(sf)
-library(fasterize)
-library(geojsonio)
+library(tidyverse)
+library(parallel)
+library(pbapply)
 
-r <- raster('macav2metdata_huss_bcc-csm1-1_r1i1p1_historical_1950_1954_CONUS_daily.nc')
-er <- extent(r)
-extent(r) <- c(er@xmin - 360, 
-               er@xmax - 360, 
-               er@ymin, 
-               er@ymax)
-geojson_path <- 'nps.geojson'
-if (!file.exists(geojson_path)) {
-  url <- 'http://open-fedmaps.opendata.arcgis.com/datasets/8b98df1300b749eabc3142864bb9a119_0.geojson'
-  download.file(url, destfile = geojson_path)
+mask_filenames <- list.files('data/masks', 
+                             pattern = '\\.tif$', 
+                             full.names = TRUE) 
+masks <- mask_filenames %>%
+  lapply(raster::raster)
+mask_names <- basename(mask_filenames) %>%
+  gsub(pattern = '\\.tif', replacement = '', x = .)
+names(masks) <- mask_names
+
+outdir <- file.path('data', 'nps')
+dir.create(outdir, showWarnings = FALSE)
+
+d <- read_delim('data/macav2metdata_urls.txt', delim='\n', col_names = 'url')
+
+
+process_file <- function(i) {
+  # download and process a  MACA file to create cropped files for each park
+  # check whether output already exists
+  destf <- trimws(basename(d$url[i]))
+  out_filenames <- paste(names(masks), 
+                         gsub('\\.nc$', '.tif', destf), 
+                         sep = '_') %>%
+    file.path(outdir, .)
+  files_already_exist <- all(file.exists(out_filenames))
+  if (files_already_exist) {
+    return(NULL)
+  }
+  
+  # if output doesn't exist yet, create it
+  download.file(d$url[i], destfile = destf)
+  
+  r <- stack(destf)
+  er <- extent(r)
+  extent(r) <- c(er@xmin - 360, 
+                 er@xmax - 360, 
+                 er@ymin, 
+                 er@ymax)
+  projection(r) <- "+proj=longlat +ellps=WGS84 +no_defs"
+  
+  # iterate over the masks and save geotiffs
+  for (j in seq_along(masks)) {
+    r %>%
+      raster::crop(masks[[j]]) %>%
+      writeRaster(filename = out_filenames[j])
+  }
+  unlink(destf)
 }
 
-parks <- geojson_read(geojson_path, what = "sp")
-poly <- subset(parks, !(STATE %in% c('MP', 'HI', 'PR', 'GU', 'AK', 'VI', 'AS'))) %>%
-  spTransform(projection(r))
 
-plot(r)
-plot(poly, add = TRUE)
-
-
-# Filter out polygons that are out of the bounds of MACA ------------------
-rpoly <- list()
-all_na <- rep(NA, nrow(poly))
-pb <- txtProgressBar(max = nrow(poly), style = 3)
-for (i in 1:nrow(poly)) {
-  rpoly[[i]] <- fasterize(as(poly[i, ], 'sf'), r, field = 'Shape__Area')
-  all_na[i] <- all(is.na(raster::values(mask(r, rpoly[[i]]))))
-  setTxtProgressBar(pb, i)
-}
-polys_to_extract <- rpoly[!all_na]
-names(polys_to_extract) <- as.character(poly$UNIT_NAME[!all_na])
-
-
-# For in-bounds polygons, extract a raster --------------------------------
-r_list <- vector(mode = 'list', length = length(polys_to_extract))
-names(r_list) <- names(polys_to_extract)
-for(i in seq_along(polys_to_extract)){
-  r_list[[i]] <- trim(mask(r, polys_to_extract[[i]]))
-}
-
-idx <- sample(length(r_list), size = 1)
-plot(r_list[[idx]], main = names(r_list)[idx])
-
+cl <- makeCluster(detectCores())
+clusterExport(cl, 'outdir')
+clusterExport(cl, 'masks')
+clusterExport(cl, 'd')
+clusterEvalQ(cl, library(raster))
+clusterEvalQ(cl, library(magrittr))
+out <- pblapply(as.list(1:nrow(d)), process_file, cl = cl)
+stopCluster(cl)
